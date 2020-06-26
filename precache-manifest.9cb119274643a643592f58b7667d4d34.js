5886a7df66b21d7253cf5ef9772b565617b2a797
Gkoushik <koushikganni@gmail.com>
2020-06-26 18:35:05 +0530

    Updates

---
+++ precache-manifest.9cb119274643a643592f58b7667d4d34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07d126d70f119054ea259d51fce09f0",
+    "url": "/Visulaizer/index.html"
+  },
+  {
+    "revision": "b5e81a67f0a73bfcee02",
+    "url": "/Visulaizer/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9b5b68c3cc3c0e866f7f",
+    "url": "/Visulaizer/static/css/main.0163c4fe.chunk.css"
+  },
+  {
+    "revision": "b5e81a67f0a73bfcee02",
+    "url": "/Visulaizer/static/js/2.681caf18.chunk.js"
+  },
+  {
+    "revision": "6a1f60932d4b758cc1944d93356f455d",
+    "url": "/Visulaizer/static/js/2.681caf18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5b68c3cc3c0e866f7f",
+    "url": "/Visulaizer/static/js/main.530ff3a4.chunk.js"
+  },
+  {
+    "revision": "2c6ecda295185fc63ff1",
+    "url": "/Visulaizer/static/js/runtime-main.602acde0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Visulaizer/precache-manifest.98da0a08339613a9ce6204a3c666af5f.js"
+  "/Visulaizer/precache-manifest.9cb119274643a643592f58b7667d4d34.js"
 );
 
 self.addEventListener('message', (event) => {
